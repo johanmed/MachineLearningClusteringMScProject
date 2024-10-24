90104a33db9d5571aee7fc8852e8418bfc7af84b
Johannes Medagbe <johanmedagbe@gmail.com>
2024-10-24 20:21:23 +0300

    deep_learning_clustering_hits.py and deep_learning_clustering_qtl.py scripts written and functional, respective outputs generated

--- scripts/deep_learning_clustering_hits.py
+++ scripts/deep_learning_clustering_hits.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 """
 Summary:
-This script contains code to do clustering using neural networks (shallow/deep learning)
+This script contains code to do clustering using neural networks (deep learning)
 Dependencies: 
 - vector_data.py -> data, preprocessing_hits
 Neural networks are used to get clustering and to predict description or trait category of validation data
@@ -15,17 +15,18 @@ from vector_data import X_train, X_valid, X_test, preprocessing_hits
 
 import numpy as np
 
-y_train=np.array(X_train['desc'][:5000])
+y_train=X_train['desc'][:5000]
 
-X_train=np.array(X_train[['one_hot_desc1', 'one_hot_desc2', 'one_hot_desc3', 'p_lrt', 'chr_num', 'pos']][:5000]) # select 5000 first columns of combined and transformed columns
+X_train=X_train[['one_hot_desc1', 'one_hot_desc2', 'one_hot_desc3', 'p_lrt', 'chr_num', 'pos']][:5000] # select 5000 first columns of combined and transformed columns
 
-y_valid=np.array(X_valid['desc'][:5000])
+y_valid=X_valid['desc'][:5000]
 
-X_valid=np.array(X_valid[['one_hot_desc1', 'one_hot_desc2', 'one_hot_desc3', 'p_lrt', 'chr_num', 'pos']][:5000]) # same
+X_valid=X_valid[['one_hot_desc1', 'one_hot_desc2', 'one_hot_desc3', 'p_lrt', 'chr_num', 'pos']][:5000] # same
 
-y_test=np.array(X_test['desc'][:5000])
+y_test=X_test['desc'][:5000]
 
-X_test=np.array(X_test[['one_hot_desc1', 'one_hot_desc2', 'one_hot_desc3', 'p_lrt', 'chr_num', 'pos']][:5000]) # same
+X_test=X_test[['one_hot_desc1', 'one_hot_desc2', 'one_hot_desc3', 'p_lrt', 'chr_num', 'pos']][:5000] # same
+
 
 
 # 2. Select the 2 columns, do clustering and plot
@@ -33,11 +34,14 @@ X_test=np.array(X_test[['one_hot_desc1', 'one_hot_desc2', 'one_hot_desc3', 'p_lr
 import tensorflow as tf # import DBSCAN class for clustering
 import matplotlib.pyplot as plt # import plot manager
 import os
-from sklearn.metrics import classification_report
+import numpy as np
+import pandas as pd
 from sklearn.pipeline import Pipeline
+from tensorflow.keras.utils import to_categorical
 
 
 out_dir=os.path.abspath('../output/') # define directory to save plots to
+
 
 
 class Columns2Clustering:
@@ -50,29 +54,42 @@ class Columns2Clustering:
         self.training=training
         self.validation=validation
         self.test=test
+    
+    
+    def get_features(self):
+        """
+        Extract 2 PCA from preprocessing_hits pipeline
+        """
+        preprocessed_training=preprocessing_hits.fit_transform(self.training)
+        preprocessed_validation=preprocessing_hits.transform(self.validation)
+        preprocessed_test=preprocessing_hits.transform(self.test)
+        
+        return preprocessed_training, preprocessed_validation, preprocessed_test
+        
 
     def perform_neural_clustering(self):
         """
         Perform neural clustering on 2 features columns
         """
         neural_model_unsup=tf.keras.models.Sequential([
-        tf.keras.layers.Dense(units=2, activation=tf.nn.relu, input_dim=2),
-        tf.keras.layers.Dropout(0.2),
-        tf.keras.layers.Dense(units=2, activation=tf.nn.relu),
-        tf.keras.layers.Dropout(0.2),
-        tf.keras.layers.Dense(units=2, activation=tf.nn.relu),
-        tf.keras.layers.Dropout(0.2),
-        tf.keras.layers.Dense(units=2, activation=tf.nn.relu),
-        tf.keras.layers.Dropout(0.2),
-        tf.keras.layers.Dense(units=2, activation=tf.nn.relu),
-        tf.keras.layers.Dropout(0.2),
-        tf.keras.layers.Dense(units=2, activation=tf.nn.softmax),
+        tf.keras.Input(shape=(2,)),
+        tf.keras.layers.Dense(units=5, activation='linear'),
+        tf.keras.layers.Dense(units=5, activation='linear'),
+        tf.keras.layers.Dense(units=5, activation='linear'),
+        tf.keras.layers.Dense(units=5, activation='linear'),
+        tf.keras.layers.Dense(units=5, activation='linear'),
+        tf.keras.layers.Dense(units=5, activation='linear'),
+        tf.keras.layers.Dense(units=5, activation='linear'),
+        tf.keras.layers.Dense(units=5, activation='linear'),
+        tf.keras.layers.Dense(units=5, activation='linear'),
+        tf.keras.layers.Dense(units=5, activation='linear'),
+        tf.keras.layers.Dense(units=5, activation=tf.nn.softmax),
         ])
-        neural_clustering=Pipeline([('preprocessing_hits', preprocessing_hits), ('neural_unsupervised', neural_model_unsup])
-        neural_clustering.compile(optimizer='adam', loss='mean_squared_error', metrics=['accuracy'])
-        neural_clustering.fit(x=self.training, epochs=20)
+        neural_clustering=Pipeline([('preprocessing_hits', preprocessing_hits), ('neural_unsupervised', neural_model_unsup)])
+        neural_clustering[1].compile(optimizer='adam', loss='kld', metrics=['accuracy'])
+        neural_clustering.fit(self.training, y_train, neural_unsupervised__epochs=50)
         #print('The labels for the first 5 training data are: ', dbscan_clustering.labels_[:5]) # check labels of first 5 training data
-        
+
         return neural_clustering
     
     
@@ -82,11 +99,18 @@ class Columns2Clustering:
         Generate actual visualization of clusters
         Save figure
         """
+        y_pred_unsup_train=neural_clustering.predict(X_train)
         
-        y_clustering_pred_training=neural_clustering.predict(X_train)
+        clusters_unsup_train=[]
+        for i in y_pred_unsup_train:
+            temp=[]
+            for (x,y) in enumerate(i):
+                if y==max(i):
+                    temp.append(x)
+            clusters_unsup_train.append(temp[0])
         
         plt.figure(figsize=(10, 10))
-        plt.scatter(neural_clustering[0][0], neural_clustering[0][1], c=y_clustering_pred_training)
+        plt.scatter(X_train[:, 0], X_train[:, 1], c=clusters_unsup_train)
         plt.xlabel("PCA 1", fontsize=10)
         plt.ylabel("PCA 2", fontsize=10, rotation=90)
         plt.savefig(os.path.join(out_dir, f"Project_PCA_neural_clustering_result_by_hits"))
@@ -98,79 +122,73 @@ class Columns2Clustering:
         """
         Use neural networks to predict clustering on validation set
         """
-        y_clustering_pred_valid=neural_clustering.predict(X_valid)
+        y_pred_unsup_valid=neural_clustering.predict(X_valid)
         
-        def extract_dist(features, labels):
-           """
-           Takes dbscan labels
-           Calculates the distance of each data to cluster
-           Returns numpy arrays of distances and corresponding indices
-           """
-           # Get centers
-           clusters_centers=[]
-           for label in set(labels):
-               if label != -1:
-                   cluster_center=np.mean(features[labels==label], axis=0)
-                   clusters_centers.append(cluster_center)
+        clusters_unsup_valid=[]
+        for i in y_pred_unsup_valid:
+            temp=[]
+            for (x,y) in enumerate(i):
+                if y==max(i):
+                    temp.append(x)
+            clusters_unsup_valid.append(temp[0])
         
-           # Calculate all distances to each cluster center
-           all_distances=np.zeros((len(features), len(clusters_centers)))
-           for i, center in enumerate(clusters_centers):
-               all_distances[:, i]=np.linalg.norm(features-center, axis=1)
-       
-           # select distance to the nearest neighbor only
-           distance=[]
-           for element in all_distances:
-               distance.append(min(element))
-       
-           return np.array(distance)
-            
-        y_dist=extract_dist(X_valid, y_clustering_pred_valid) # get distances and indices to nearest clusters obtained for training data
-        y_clustering_pred_valid[y_dist>0.2]=-1 # detect anomalies by setting maximum distance allowed between instance and nearest cluster to 0.2 (can be changed)
-        #print('The labels for the first 5 validation data are: \n', y_pred[:5]) # check labels for the first 5 validation data
-        return y_clustering_pred_valid
+        #print('The labels for the first 5 validation data are: \n', y_pred_unsup_valid[:5]) # check labels for the first 5 validation data
+        
+        return y_pred_unsup_valid
 
 
      
-    def extract_features_target_relationship(X_train, y_train, X_valid, y_valid):
+    def extract_features_target_relationship(self, y_train, y_valid):
         """
         Find relationships between 2 columns selected (features) and description (target) using neural networks
         Assign to each observation a description to know the type of trait -> supervised learning
         """
         neural_model_sup=tf.keras.models.Sequential([
-        tf.keras.layers.Dense(units=2, activation=tf.nn.relu, input_dim=2),
-        tf.keras.layers.Dropout(0.2),
-        tf.keras.layers.Dense(units=2, activation=tf.nn.relu),
-        tf.keras.layers.Dropout(0.2),
-        tf.keras.layers.Dense(units=2, activation=tf.nn.relu),
-        tf.keras.layers.Dropout(0.2),
-        tf.keras.layers.Dense(units=2, activation=tf.nn.relu),
-        tf.keras.layers.Dropout(0.2),
-        tf.keras.layers.Dense(units=2, activation=tf.nn.relu),
-        tf.keras.layers.Dropout(0.2),
+        tf.keras.Input(shape=(2,)),
+        tf.keras.layers.Dense(units=5, activation=tf.nn.relu),
+        tf.keras.layers.Dense(units=5, activation=tf.nn.relu),
+        tf.keras.layers.Dense(units=5, activation=tf.nn.relu),
+        tf.keras.layers.Dense(units=5, activation=tf.nn.relu),
+        tf.keras.layers.Dense(units=5, activation=tf.nn.relu),
+        tf.keras.layers.Dense(units=5, activation=tf.nn.relu),
+        tf.keras.layers.Dense(units=5, activation=tf.nn.relu),
+        tf.keras.layers.Dense(units=5, activation=tf.nn.relu),
+        tf.keras.layers.Dense(units=5, activation=tf.nn.relu),
+        tf.keras.layers.Dense(units=5, activation=tf.nn.relu),
         tf.keras.layers.Dense(units=3, activation=tf.nn.softmax),
         ])
-        neural_assign=Pipeline([('preprocessing_hits', preprocessing_hits), ('neural_supervised', neural_model_sup])
-        neural_assign.compile(optimizer='adam', loss='mean_squared_error', metrics=['accuracy'])
-        neural_assign.fit(x=X_train, y=y_train, epochs=20)
-        y_supervised_pred=neural_assign.predict(X_valid)
+        
+        
+        # Convert target variables to one-hot encoded format
+
+        y_train = to_categorical(y_train, num_classes=3)
+
+        y_valid = to_categorical(y_valid, num_classes=3)
+
+
+
+        neural_assign=Pipeline([('preprocessing_hits', preprocessing_hits), ('neural_supervised', neural_model_sup)])
+        neural_assign[1].compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+        neural_assign.fit(self.training, y_train, neural_supervised__epochs=50)
+        y_supervised_pred=neural_assign.predict(self.validation)
+        
+        
         #print('The prediction for the first 5 validation data is :', y_pred[:5])
-        print(classification_report(y_valid, y_supervised_pred))
         
         return y_supervised_pred
 
 
-    def visualize_plot_annotation(neural_clustering, X_valid, y_supervised_pred, type_anno):
+    def visualize_plot_annotation(X_valid, y_supervised_pred, type_anno):
         """
         Regenerate visualization for clustering adding annotation of description or original trait category to each observation
         Save figure
         """
         plt.figure(figsize=(10, 10))
-        plt.scatter(neural_clustering[0][0], neural_clustering[0][1], c=y_supervised_pred)
+        plt.scatter(X_valid[:, 0], X_valid[:, 1], c=y_supervised_pred)
         plt.xlabel("PCA 1", fontsize=10)
         plt.ylabel("PCA 2", fontsize=10, rotation=90)
         plt.colorbar(label='Original trait category', spacing='uniform', values=[0, 1, 2])
-        plt.savefig(os.path.join(out_dir, f"Project_PCA_DBSCAN_clustering_KNN_{type_anno}_annotation_result_by_hits"))
+        plt.savefig(os.path.join(out_dir, f"Project_PCA_neural_clustering_{type_anno}_annotation_result_by_hits"))
         plt.show()
 
      
@@ -179,12 +197,22 @@ class Columns2Clustering:
 # Main
 
 clustering_task=Columns2Clustering(X_train, X_valid, X_test)
-actual_clustering=clustering_task.perform_dbscan_clustering()
-Columns2Clustering.visualize_plot(Columns2Clustering.plot_dbscan, actual_clustering, X_train)
-prediction_clusters=Columns2Clustering.predict_dbscan_clustering(actual_clustering, X_valid)
-extracted_annotation=Columns2Clustering.extract_features_target_relationship(X_train, y_train, X_valid, y_valid)
-Columns2Clustering.visualize_plot_annotation(X_valid, extracted_annotation, 'predicted')
-Columns2Clustering.visualize_plot_annotation(X_valid, y_valid, 'actual')
+
+X_train_features, X_valid_features, X_test_features=clustering_task.get_features()
+
+actual_clustering=clustering_task.perform_neural_clustering()
+
+Columns2Clustering.visualize_plot(actual_clustering[1], X_train_features)
+
+Columns2Clustering.visualize_plot(actual_clustering[1], X_valid_features)
+
+prediction_clusters=Columns2Clustering.predict_neural_clustering(actual_clustering[1], X_valid_features)
+
+extracted_annotation=clustering_task.extract_features_target_relationship(y_train, y_valid)
+
+Columns2Clustering.visualize_plot_annotation(X_valid_features, extracted_annotation, 'predicted')
+
+Columns2Clustering.visualize_plot_annotation(X_valid_features, y_valid, 'actual')
 
 
 
